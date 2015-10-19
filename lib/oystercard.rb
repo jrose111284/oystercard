36fb7d910d47f7c56cb174ff793950ef18b4b1c7
blancopado <pablo.dominguezblanco@gmail.com>
2015-10-19 17:34:11 +0200

    4th challenge

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,9 @@
 class Oystercard
+	attr_reader :money
 
+	DEFAULT_BALANCE = 0
+	
+	def initialize(money=DEFAULT_BALANCE)
+		@money = money
+	end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
-
+	describe '#initialize' do
+		it 'has a default of 0' do
+			expect(subject.money).to eq(0)
+		end
+	end
 end
