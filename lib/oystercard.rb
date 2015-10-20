a9b5718ddadfc287a09e61f5e803f6123bf9bd9c
blancopado <pablo.dominguezblanco@gmail.com>
2015-10-20 14:17:01 +0200

    Chapter 8 finished

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,24 +1,28 @@
 class Oystercard
-	attr_reader :money, :in_journey
+  attr_reader :money, :in_journey
   LIMIT = 90.00
 
-	def initialize
-		@money = 0
+  def initialize
+	@money = 0
     @in_journey = false
-	end
+  end
+  
+  def top_up(amount)
+	fail "The limit is 90" if money + amount > LIMIT
+	@money += amount
+  end
 
-	def top_up(amount)
-		fail "The limit is 90" if money + amount > LIMIT
-		@money += amount
-	end
+  def deduct(amount)
+	@money -= amount
+  end
 
-	def deduct(amount)
-		@money -= amount
-	end
+  def touch_in
+	@in_journey = true
+  end
 
-	def touch_in
-		@in_journey = true
-	end
+  def touch_out
+  	@in_journey = false
+  end
 
   def in_journey?
     @in_journey
