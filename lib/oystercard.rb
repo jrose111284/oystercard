c05cea8a8380b27ebdcecaa19dfc62b54330b254
Norm Fasey <norm@fasey.net>
2015-10-20 12:05:41 +0100

    Added tests for touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,14 @@
 class Oystercard
 	attr_reader :money, :in_journey
-	LIMIT = 91
-	
+  LIMIT = 90.00
+
 	def initialize
 		@money = 0
-		@in_journey = false
+    @in_journey = false
 	end
 
 	def top_up(amount)
-		fail "The limit is 90" if money + amount >= LIMIT
+		fail "The limit is 90" if money + amount > LIMIT
 		@money += amount
 	end
 
@@ -16,11 +16,11 @@ class Oystercard
 		@money -= amount
 	end
 
-	def in_journey?
-		@in_journey
+	def touch_in
+		@in_journey = true
 	end
 
-	def touch_in
-		@in_journey = false
-	end
+  def in_journey?
+    @in_journey
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,41 +1,51 @@
 require 'oystercard'
 
 describe Oystercard do
-	describe '#initialize' do
-		it 'has a default of 0' do
-			expect(subject.money).to eq(0)
-		end
-	end
+  describe '#initialize' do
+    it 'has a default of 0' do
+      expect(subject.money).to eq(0)
+    end
+  end
 
-	describe '#top_up' do
-		it {is_expected.to respond_to(:top_up).with(1).argument }
+  describe '#top_up' do
+    it {is_expected.to respond_to(:top_up).with(1).argument }
 
-		it 'can top up the balance' do
-			expect{ subject.top_up 1 }.to change{ subject.money }.by 1
-		end
+    it 'can top up the balance' do
+      expect{ subject.top_up 1.00 }.to change{ subject.money }.by 1.00
+    end
 
-		it 'limit is 90' do
-			expect{subject.top_up(described_class::LIMIT)}.to raise_error("The limit is 90")
-		end
-	end
+    it 'limit is 90' do
+      subject.top_up(described_class::LIMIT)
+      expect{subject.top_up(1.00)}.to raise_error("The limit is 90")
+    end
+  end
 
-	describe '#deduct' do
-		it {is_expected.to respond_to(:deduct).with(1).argument }
+  describe '#deduct' do
+    it {is_expected.to respond_to(:deduct).with(1).argument }
 
-		it 'deduct money' do
-			expect{subject.deduct 1 }.to change{ subject.money }.by -1
-		end
-	end
+    it 'deduct money' do
+      expect{subject.deduct 1.00 }.to change{ subject.money }.by -1.00
+    end
+  end
 
-	describe '#touch_in' do
-    	it {is_expected.to respond_to (:in_journey?) }
-		it {is_expected.to respond_to (:touch_in)}
-		it 'a new oystercard is not an journey' do
-			expect(subject.in_journey?).to be false
-		end
+  describe '#touch_in' do
+    it {is_expected.to respond_to (:in_journey?) }
+    it {is_expected.to respond_to (:touch_in)}
+    it 'a new oystercard is not an journey' do
+      expect(subject.in_journey?).to be false
+    end
 
-    	it 'changes the in journey status to true when touched in' do
-    		#expect {subject.touch_in}
-		end
-	end
+    it 'changes the in journey status to true when touched in' do
+      subject.touch_in
+      expect(subject.in_journey?).to be true
+    end
+  end
+  describe '#touch_out' do
+    it { is_expected.to respond_to(:touch_out)}
+
+    it 'changes the in journey status to false when touched out' do
+      subject.touch_out
+      expect(subject.in_journey?).to be false
+    end
+  end
 end
